9adb5e3f00078deaf24ffd8d25eed2f666090ad9
TongLongDev <tonglong.dev@gmail.com>
2026-03-25 11:15:41 +0700

    tailwindcss setup

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,17 @@ import Profile from "./pages/Profile";
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/auth/:pathname" element={<Auth />} />
-        <Route path="/account/:pathname" element={<Account />} />
-      </Routes>
+      <div className="min-h-screen flex flex-col">
+        <main className="flex-1">
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/onboarding" element={<Onboarding />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/auth/:pathname" element={<Auth />} />
+            <Route path="/account/:pathname" element={<Account />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
